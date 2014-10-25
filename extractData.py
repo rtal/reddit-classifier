d435599c4b21a6389a3d6add8c6e6d89548286a8
rtal <talreja.rohit@gmail.com>
2014-10-25 12:26:56 -0700

    Added extractData.py, which turns an arbitary number of messy csv files into a single text file with JSON encoded post title and subreddit pairs

---
+++ extractData.py
@@ -0,0 +1,30 @@
+import sys
+import csv
+import json
+
+
+""" Input: many files from the Reddit database input as command line arguments
+    Output: text file with only the relevant information from each file in a
+            JSON format ({'subreddit': [name of subreddit], 'title': [post title]})
+    Note that the data.txt file is overwritten each time this script is called.
+    This behavior may be modified in the future so that new data is appended.
+"""
+with open('data.txt', 'wb') as dataFile:
+    dataFile.truncate()
+    for fileName in sys.argv[1:]:
+        subreddit = fileName.strip('.csv')
+        with open(fileName, 'rb') as redditFile:
+            redditReader = csv.reader(redditFile)
+            for post in redditReader:
+                dataFile.write(json.dumps({'title': post[4], 'subreddit': subreddit}) + '\n')
+
+
+
+""" Open the data.txt file and de-serialize each JSON string representing a single
+    title-subreddit pair. Print the title. This is the main logic that will be used
+    for reading the data file and training the weight vector later on.
+"""
+with open('data.txt', 'rb') as dataFile:
+    for line in dataFile:
+        dataPoint = json.loads(line)
+        print dataPoint['title']
