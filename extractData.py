11d1c87cab0a52167bac879ffa81336933e665c7
rtal <talreja.rohit@gmail.com>
2014-11-02 14:29:54 -0800

    Add ability to create train and test files

--- extractData.py
+++ extractData.py
@@ -10,22 +10,30 @@ def parseData(fileList):
         Note that the data.txt file is overwritten each time this script is called.
         This behavior may be modified in the future so that new data is appended.
     """
-    with open('Data.txt', 'wb') as dataFile:
-        dataFile.truncate()
-        for fileName in fileList:
-            subreddit = fileName.strip('.csv').strip('data/')
-            with open(fileName, 'rb') as redditFile:
-                redditReader = csv.reader(redditFile)
-                for post in redditReader:
-                    dataFile.write(json.dumps({'title': post[4], 'subreddit': subreddit}) + '\n')
+    cutoff = 10
+    with open('TrainData.txt', 'wb') as trainFile:
+        with open('TestData.txt', 'wb') as testFile:
+            trainFile.truncate()
+            testFile.truncate()
+            for fileName in fileList:
+                i = 0
+                subreddit = fileName.strip('.csv').strip('data/')
+                with open(fileName, 'rb') as redditFile:
+                    redditReader = csv.reader(redditFile)
+                    for post in redditReader:
+                        if i % cutoff == 0:
+                            testFile.write(json.dumps({'title': post[4], 'subreddit': subreddit}) + '\n')
+                        else:
+                            trainFile.write(json.dumps({'title': post[4], 'subreddit': subreddit}) + '\n')
+                        i += 1
 
 
-def printData():
-    """ Open the data.txt file and de-serialize each JSON string representing a single
+def printData(fileName):
+    """ Open the file and de-serialize each JSON string representing a single
         title-subreddit pair. Print the title. This is the main logic that will be used
         for reading the data file and training the weight vector later on.
     """
-    with open('data.txt', 'rb') as dataFile:
+    with open(fileName, 'rb') as dataFile:
         for line in dataFile:
             dataPoint = json.loads(line)
             print dataPoint['title']
