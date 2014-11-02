0d9dbf59696612081b6f3b4e97b44b37a254c773
Joseph Baena <jbaena@stanford.edu>
2014-11-02 14:43:16 -0800

    logistic regression stuff

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -5,15 +5,37 @@ import extractData
 import FeatureExtractor
 import util
 
-def makeTrainingSet(subredditFiles):
-	extractData.parseData(subredditFiles)
-	with open('data.txt', 'r') as dataFile:
-		
 
+def train(trainingSet, subredditLabels):
+	numIterations = 20
+	eta = 0.05
 
+	#dictionary of dictionaries (weights)
+	weightDict = {}
 
+	for i in range(numIterations):
+		for example in trainingSet:
+			example = json.loads(example)
+			title = example['title']
+			subreddit = example['subreddit']
 
+			features = FeatureExtractor.extractFeatures(title)
+
+			currentWeightVector = weightDict[subreddit]
+
+			maxScore = float('-inf')
+			for label in subredditLabels:
+				weightVector = weightDict[label]
+
+				score = dotProduct(weightVector, features) - dotProduct(currentWeightVector, features)
+				if label != subreddit:
+					score = score + 1
+
+				if score > maxScore:
+					maxScore = score
+
+			currentWeightVector = currentWeightVector - eta * 
 
 
 if __name__ == '__main__':
-	makeTrainingSet(sys.argv[1:])
+	train(trainingSet, subredditLabels)
