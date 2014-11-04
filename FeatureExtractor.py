7f77b917ae9be222261246256d1cc8a7faa7903a
rtal <talreja.rohit@gmail.com>
2014-11-03 17:49:32 -0800

    Changes feature extractor to ignore stop words. For now these are: the, a, an, or, and, is, which, on. Accuracy improves 2-4%

--- FeatureExtractor.py
+++ FeatureExtractor.py
@@ -1,12 +1,29 @@
 import json
 import collections
 
+IGNORED_WORDS = [
+    'the', 'The',
+    'a', 'A',
+    'an', 'An',
+    'or', 'Or',
+    'is', 'Is',
+    'and',
+    'which',
+    'on'
+]
+
 def extractFeatures(title):
-	"""
-	Get word features for title
-	Use word frequencies (to start)
-	Return dictionary
-	"""
-	tokens = title.split()
-	featureVector = collections.Counter(tokens)
-	return featureVector
+    """
+    Get word features for title
+    Use word frequencies (to start)
+    Return dictionary
+    """
+    tokens = title.split()
+
+    featureVector = collections.Counter(tokens)
+
+    # Remove the IGNORED_WORDS from the feature vector
+    for word in IGNORED_WORDS:
+        featureVector.pop(word, None)
+
+    return featureVector
